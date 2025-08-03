9259b811bb32469635adfb035590ce86a42a1451
ShiYue1026 <shiyue20011026@126.com>
2025-08-03 22:43:15 +0800

    initial

---
+++ src/main/java/com/shiyue/TicketGrabbingAiApplication.java
@@ -0,0 +1,13 @@
+package com.shiyue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketGrabbingAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketGrabbingAiApplication.class, args);
+    }
+
+}
